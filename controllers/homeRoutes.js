e8430d6ee21c54c8713afcda5f697fef2ed3ba03
00rest <119826790+00rest@users.noreply.github.com>
2023-05-07 22:46:01 -0400

    half way there

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,75 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data (WORKS WITH INSOMNIA)
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['user_name', ],
+        },
+      ],
+    });
+
+    res.status(200).json(postData);
+
+    // Serialize data so the template can read it
+    //const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    // res.render('homepage', { 
+    //   projects, 
+    //   logged_in: req.session.logged_in 
+    // });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Comment on a post withAuth (WORKS WITH INSOMNIA)
+router.post('/dashboard', async (req, res) => {
+  try {
+    console.log(req.body);
+    Comment.create(req.body)
+    .then((result) => res.status(200).json(result))
+  
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err)
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
---
+++ models/Comment.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    comment_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'post_id',
+      },
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'user_id',
+      },
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
